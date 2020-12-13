68cf76d73f38135144416ed03a2dac8adf24df6a
pavelborisov <pavel.borisofff@gmail.com>
2020-12-13 10:08:49 +0300

    test periods

--- yrno/hours_list.py
+++ yrno/hours_list.py
@@ -1,46 +1,46 @@
-# import datetime
-#
-#
-# def get_parsed_time(diff_to_utc: int = -3):
-#     template = '{}-{:0>2}-{:0>2}T{:0>2}:00:00Z'
-#     today = datetime.datetime.now() + datetime.timedelta(hours=diff_to_utc)
-#     next_one = today + datetime.timedelta(days=1)
-#     next_two = today + datetime.timedelta(days=2)
-#     next_three = today + datetime.timedelta(days=3)
-#
-#     times = {
-#         range(0, 13):
-#             [
-#                 template.format(today.year, today.month, today.day, today.hour),  # next one
-#                 template.format(today.year, today.month, today.day, 12 + diff_to_utc),  # today day
-#                 template.format(next_one.year, next_one.month, next_one.day, 6 + diff_to_utc),  # next one
-#                 template.format(next_one.year, next_one.month, next_one.day, 12 + diff_to_utc),  # next one day
-#                 template.format(next_two.year, next_two.month, next_two.day, 6 + diff_to_utc),  # next two
-#                 template.format(next_two.year, next_two.month, next_two.day, 12 + diff_to_utc),  # next two day
-#                 template.format(next_three.year, next_three.month, next_three.day, 12)  # next three
-#             ],
-#         range(12, 19):
-#             [
-#                 template.format(today.year, today.month, today.day, today.hour),
-#                 template.format(today.year, today.month, today.day, today.hour),  # next one
-#                 template.format(next_one.year, next_one.month, next_one.day, 6 + diff_to_utc),  # next one
-#                 template.format(next_one.year, next_one.month, next_one.day, 12 + diff_to_utc),  # next one day
-#                 template.format(next_two.year, next_two.month, next_two.day, 6 + diff_to_utc),  # next two
-#                 template.format(next_two.year, next_two.month, next_two.day, 12 + diff_to_utc),  # next two day
-#                 template.format(next_three.year, next_three.month, next_three.day, 12)  # next three
-#             ],
-#         range(18, 25):
-#             [
-#                 template.format(today.year, today.month, today.day, today.hour),  # next one (no next one day)
-#                 template.format(next_one.year, next_one.month, next_one.day, 6 + diff_to_utc),  # next one
-#                 template.format(next_one.year, next_one.month, next_one.day, 12 + diff_to_utc),  # next one day
-#                 template.format(next_two.year, next_two.month, next_two.day, 6 + diff_to_utc),  # next two
-#                 template.format(next_two.year, next_two.month, next_two.day, 12 + diff_to_utc),  # next two day
-#                 template.format(next_three.year, next_three.month, next_three.day, 12)  # next three
-#             ]
-#     }
-#
-#     for period in times:
-#         if today.hour in period:
-#             return times[period]
-#
+import datetime
+
+
+def get_strftime(diff_to_utc: int = -3) -> list[str]:
+    """
+    Generate keys to search exactly time periods into yr.no data
+    :param diff_to_utc:
+    :return:
+    """
+    template = '{}-{:0>2}-{:0>2}T{:0>2}:00:00Z'
+    today = datetime.datetime.now() + datetime.timedelta(hours=diff_to_utc)
+    next_one = today + datetime.timedelta(days=1)
+    next_two = today + datetime.timedelta(days=2)
+    next_three = today + datetime.timedelta(days=3)
+
+    hours_ranges = {
+        range(0, 13):
+            [
+                template.format(today.year, today.month, today.day, today.hour),  # today
+                template.format(today.year, today.month, today.day, 12 + diff_to_utc),  # today day
+                template.format(next_one.year, next_one.month, next_one.day, 6 + diff_to_utc),  # next one
+                template.format(next_one.year, next_one.month, next_one.day, 12 + diff_to_utc),  # next one day
+                template.format(next_two.year, next_two.month, next_two.day, 6 + diff_to_utc),  # next two
+                template.format(next_two.year, next_two.month, next_two.day, 12 + diff_to_utc),  # next two day
+                template.format(next_three.year, next_three.month, next_three.day, 12)  # next three
+            ],
+        range(12, 25):
+            [
+                template.format(today.year, today.month, today.day, today.hour),  # next one
+                template.format(today.year, today.month, today.day, today.hour),  # today day
+                template.format(next_one.year, next_one.month, next_one.day, 6 + diff_to_utc),  # next one
+                template.format(next_one.year, next_one.month, next_one.day, 12 + diff_to_utc),  # next one day
+                template.format(next_two.year, next_two.month, next_two.day, 6 + diff_to_utc),  # next two
+                template.format(next_two.year, next_two.month, next_two.day, 12 + diff_to_utc),  # next two day
+                template.format(next_three.year, next_three.month, next_three.day, 12)  # next three
+            ]
+    }
+
+    for hour_range in hours_ranges:
+        if today.hour in hour_range:
+            return hours_ranges[hour_range]
+
+
+d = get_strftime()
+from pprint import pprint
+pprint(d)
--- yrno/yrno.py
+++ yrno/yrno.py
@@ -5,8 +5,8 @@ import requests
 from typing import Union
 
 # SETTINGS
-TZ = 3
-URL = 'https://api.met.no/weatherapi/locationforecast/2.0/complete'
+TZ = 3  # Difference between local and UTC time, hours
+URL = 'https://api.met.no/weatherapi/locationforecast/2.0/complete'  # yr.no API endpoint
 USER_AGENTS_LIST = 'user_agents.txt'
 
 # LIST OF GEOLOCATIONS
@@ -23,6 +23,11 @@ WEATHER_DAY = ['weather_sky_day', 'weather_temp_day', 'wind_velocity_day', 'wind
 
 
 def get_direction(degrees: float) -> str:
+    """
+    Convert wind direction degrees to characters
+    :param degrees: 123,5
+    :return: 'SE'
+    """
     sector = int(degrees // 22.5)
     directions = {
         0: 'N',
@@ -46,12 +51,15 @@ def get_direction(degrees: float) -> str:
 
     return directions.get(sector, '-')
 
-def translate_sky(en_name: str) -> str:
+
+def translate_sky(symbol_code: str) -> str:
     """
     Translate yr.no weather types to Rosa Khutor's types
-    :param en_name:
-    :return:
+    :param en_name: 'clearsky_day' -> 'clearsky'
+    :return: 'СОЛНЦЕ' or 'None'
     """
+    en_name = symbol_code.split('_')[0]
+
     return {
         'clearsky': 'СОЛНЦЕ',
         'cloudy': 'ПАСМУРНО',
@@ -99,9 +107,10 @@ def translate_sky(en_name: str) -> str:
 
 def utc_to_local(utc_dt: datetime.datetime) -> datetime.datetime:
     """
-    Return UTC time converted to local time.
-    :param utc_dt:
-    :return:
+    Return UTC time converted to local time. Like 06:00:00 UTC == 09:00:00 GMT+3
+    :param utc_dt: datetime.datetime.utcnow() -> datetime.datetime(2020, 12, 12, 21, 49, 15, 663953)
+    :return: datetime.datetime(2020, 12, 13, 0, 50, 9, 563725,
+    tzinfo=datetime.timezone(datetime.timedelta(seconds=10800), 'MSK'))
     """
     return utc_dt.replace(tzinfo=datetime.timezone.utc).astimezone(tz=None)
 
@@ -110,7 +119,7 @@ def compare_hours(utc_time: str) -> bool:
     """
     Compare time from yr.no with current time.
     :param utc_time: str must be like '2020-12-11T21:06:52Z'
-    :return: If difference more than 1 hour return False
+    :return: True or if difference more than 1 hour return False
     """
     try:
         to_local = utc_to_local(datetime.datetime.fromisoformat(utc_time[:-1]))
@@ -121,11 +130,11 @@ def compare_hours(utc_time: str) -> bool:
         return True  # Because yr.no send current weather in first index
 
 
-def get_weather_dict(data: dict) -> list[str, Union[str, int]]:
+def get_weather_conds(data: dict) -> list[str, Union[str, int]]:
     """
     Get weather data structured yr.no json
-    :param data:
-    :return:
+    :param data: {'time': '2020-12-12T19:00:00Z', 'data': {'instant': {'details': {'air_pressure_at_sea_level': 17.0}}}}
+    :return: []
     """
     sky_period = data['data'].get('next_1_hours') or data['data'].get('next_6_hours')
     return [
@@ -135,9 +144,10 @@ def get_weather_dict(data: dict) -> list[str, Union[str, int]]:
         get_direction(data['data']['instant']['details']['wind_from_direction'])
     ]
 
-def get_parsed_time(diff_to_utc: int = -3) -> list[str]:
+
+def get_strftime(diff_to_utc: int = -3) -> list[str]:
     """
-    Func to generate keys to search exactly time period into yr.no data
+    Generate keys to search exactly time periods into yr.no data
     :param diff_to_utc:
     :return:
     """
@@ -147,10 +157,10 @@ def get_parsed_time(diff_to_utc: int = -3) -> list[str]:
     next_two = today + datetime.timedelta(days=2)
     next_three = today + datetime.timedelta(days=3)
 
-    times = {
+    hours_ranges = {
         range(0, 13):
             [
-                template.format(today.year, today.month, today.day, today.hour),  # next one
+                template.format(today.year, today.month, today.day, today.hour),  # today
                 template.format(today.year, today.month, today.day, 12 + diff_to_utc),  # today day
                 template.format(next_one.year, next_one.month, next_one.day, 6 + diff_to_utc),  # next one
                 template.format(next_one.year, next_one.month, next_one.day, 12 + diff_to_utc),  # next one day
@@ -158,19 +168,10 @@ def get_parsed_time(diff_to_utc: int = -3) -> list[str]:
                 template.format(next_two.year, next_two.month, next_two.day, 12 + diff_to_utc),  # next two day
                 template.format(next_three.year, next_three.month, next_three.day, 12)  # next three
             ],
-        range(12, 19):
+        range(12, 25):
             [
-                template.format(today.year, today.month, today.day, today.hour),
                 template.format(today.year, today.month, today.day, today.hour),  # next one
-                template.format(next_one.year, next_one.month, next_one.day, 6 + diff_to_utc),  # next one
-                template.format(next_one.year, next_one.month, next_one.day, 12 + diff_to_utc),  # next one day
-                template.format(next_two.year, next_two.month, next_two.day, 6 + diff_to_utc),  # next two
-                template.format(next_two.year, next_two.month, next_two.day, 12 + diff_to_utc),  # next two day
-                template.format(next_three.year, next_three.month, next_three.day, 12)  # next three
-            ],
-        range(18, 25):
-            [
-                template.format(today.year, today.month, today.day, today.hour),  # next one (no next one day)
+                template.format(today.year, today.month, today.day, today.hour),  # today day
                 template.format(next_one.year, next_one.month, next_one.day, 6 + diff_to_utc),  # next one
                 template.format(next_one.year, next_one.month, next_one.day, 12 + diff_to_utc),  # next one day
                 template.format(next_two.year, next_two.month, next_two.day, 6 + diff_to_utc),  # next two
@@ -179,9 +180,9 @@ def get_parsed_time(diff_to_utc: int = -3) -> list[str]:
             ]
     }
 
-    for period in times:
-        if today.hour in period:
-            return times[period]
+    for hour_range in hours_ranges:
+        if today.hour in hour_range:
+            return hours_ranges[hour_range]
 
 
 
@@ -194,8 +195,11 @@ def get_parsed_time(diff_to_utc: int = -3) -> list[str]:
 # List of local points
 
 # Read list of user agents to rotate it while requesting
+
+# Preparation to operate
 with open(USER_AGENTS_LIST, encoding='utf8') as f:
     headers_list = f.readlines()
+headers = {"User-Agent": 'for test reasons'}
 
 session = requests.session()
 
@@ -203,8 +207,7 @@ session = requests.session()
 
 # headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
 # headers = {"User-Agent": random.choice(headers_list).strip()}
-headers = {"User-Agent": 'for test reasons'}
-params = POINTS[0]['coordinates']
+
 
 
 for point in POINTS:
@@ -216,26 +219,37 @@ for point in POINTS:
     except Exception as e:
         print(e)
 
-    weather = {}
+    strftime_list = get_strftime()
+    periods = [data for data in weather_data if data.get('time') in strftime_list]
 
-    # # for data in weather_data:
-    # #     if not compare_time(data['time']):
-    # #         next(weather_data)
-    # #     else:
-    # data_now = weather_data[0]
-    if not compare_hours(weather_data[0]['time']):
-        # logger.error(f'{data_now["time"]} CHECK TIME')
-        pass
+    weather = {
+        'weather_0_yrno': {},
+        'weather_1_yrno': {},
+        'weather_2_yrno': {}
+    }
 
-    if datetime.datetime.now().hour < 6:
-        weather['weather_0_yrno'] = {k:v for k,v in zip(WEATHER_NOW, get_weather_dict(weather_data[0]))}
-        weather['weather_0_yrno'].update({k: v for k, v in zip(WEATHER_DAY, get_weather_dict(weather_data[0]))})
-    elif datetime.datetime.now().hour in range(12, 19):
-        weather['now_day'] = weather['now']
-    else:
+    for day in range(0, 2):
+        for i, period in enumerate(periods[:-1]):
+            print(day, i, period)
+            if i % 2:
+                weather[f'weather_{day}_yrno'].update({k: v for k, v in zip(WEATHER_DAY, get_weather_conds(period))})
+            else:
+                weather[f'weather_{day}_yrno'].update({k: v for k, v in zip(WEATHER_NOW, get_weather_conds(period))})
+        # {k: v for k, v in zip(WEATHER_NOW, get_weather_conds(period))}
+
+    # if not compare_hours(weather_data[0]['time']):
+    #     # logger.error(f'{data_now["time"]} CHECK TIME')
+    #     pass
+
+    # if datetime.datetime.now().hour < 6:
+    #     weather['weather_0_yrno'] = {k:v for k,v in zip(WEATHER_NOW, get_weather_conds(weather_data[0]))}
+    #     weather['weather_0_yrno'].update({k: v for k, v in zip(WEATHER_DAY, get_weather_conds(weather_data[0]))})
+    # elif datetime.datetime.now().hour in range(12, 19):
+    #     weather['now_day'] = weather['now']
+    # else:
 
 
-    print(weather['now'])
+    print(weather)
 
 
 
